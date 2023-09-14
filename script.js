9c1859e97710f5be22d2855bdcc4ef073f5b1e24
Tee <139244119+teeokh@users.noreply.github.com>
2023-09-14 21:49:21 +0100

    Change number of boxes in column
    Use dynamic CSS changes in JS to change number of boxes in each row depending on squareCount. Use math.sqrt to calculate ideal width of each box. Use temporal literals to implement calculation into flex dimensions

--- script.js
+++ script.js
@@ -2,9 +2,14 @@ const container = document.querySelector("#container"); // Select "container" el
 
 function createGrid (squareCount) {
 
+    const squareWidth = 100 / Math.sqrt(squareCount);
+
     for(i = 0; i < squareCount; i++){ // Loop 16 times to create 16 divs
+    
     const grid = document.createElement("div"); // Create div element named grid
     grid.classList.add("grid"); // Add .grid css to grid 
+    grid.style.flex = `0 0 calc(${squareWidth}% - 8px)`;
+
     container.appendChild(grid); // Append grid to container
 
     const grids = document.querySelectorAll(".grid");
