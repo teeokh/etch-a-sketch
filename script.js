3aceb72ff7e47978907610611d28325508c4f2e8
Tee <139244119+teeokh@users.noreply.github.com>
2023-09-15 12:36:11 +0100

    Random colour change on eventListeners

--- script.js
+++ script.js
@@ -5,22 +5,46 @@ function createGrid (squareCount) {
     const squareWidth = 100 / Math.sqrt(squareCount);
 
     for(i = 0; i < squareCount; i++){ // Loop 16 times to create 16 divs
+
+    const grids = document.querySelectorAll(".grid");
+
+    let red = Math.floor(Math.random()*256);
+    let green = Math.floor(Math.random()*256);
+    let blue = Math.floor(Math.random()*256);
     
     const grid = document.createElement("div"); // Create div element named grid
+    // const grid_change = document.querySelector("div");
     grid.classList.add("grid"); // Add .grid css to grid 
+    // grid_change.classList.add("grid_change");
+
     grid.style.flex = `0 0 calc(${squareWidth}% - 8px)`;
+    // grid_change.style.flex = `0 0 calc(${squareWidth}% - 8px)`;
+
+    grid.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+    
 
     container.appendChild(grid); // Append grid to container
 
-    const grids = document.querySelectorAll(".grid");
-    
     grids.forEach((grid) => {
         grid.addEventListener("mouseover", () => {
-            grid.classList.add("grid-change");
+            // grid.classList.add("grid_change");
+
+            red = Math.floor(Math.random()*256);
+            green = Math.floor(Math.random()*256);
+            blue = Math.floor(Math.random()*256);
+
+            grid.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+
+            // grid_change.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
         })
 
         grid.addEventListener("mouseout", () => {
-            grid.classList.remove("grid-change")
+            // grid.classList.remove("grid_change");
+
+            red = Math.floor(Math.random()*256);
+            green = Math.floor(Math.random()*256);
+            blue = Math.floor(Math.random()*256);
+            grid.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
         })
     })
 }
